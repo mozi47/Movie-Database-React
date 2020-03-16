c14d2d4b503c50c6b4df798c89c04bdc13d175f9
mozi47 <mozi47.shah@gmail.com>
2020-03-16 14:01:00 +0500

    update the movie code

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React,{useState} from 'react';
+import Results from "./component/Results"
+import Search from "./component/Search"
+import axios from "axios"
+import Popup from "./component/Popup"
+
+function App() {
+
+  const APIKEY="http://www.omdbapi.com/?apikey=6a3c33da"
+  const [state,setState]=useState({
+    s:"",
+    results:[],
+    selected:[]
+  })
+
+  const search=(e)=>{
+    if(e.key==="Enter"){
+    axios(APIKEY+"&s="+state.s).then(({data})=>{
+      let results=data.Search
+      setState(prevState=>{
+        return {...prevState, results:results}
+      })
+    })
+  }}
+
+  const openPopup=(id)=>{
+    axios(APIKEY + "&i=" + id).then(({ data })=>{
+        let result=data
+        //console.log(id)
+        setState(prevState=>{
+          return {...prevState, selected:result}
+        })
+    })
+  }
+
+  const closePopup=()=>{
+    setState(prevState=>{
+          return {...prevState, selected:{}}
+        })
+    }
+
+  const handleInput=(e)=>{
+    let s=e.target.value
+
+    setState(prevState=>{
+      return {...prevState,s:s}
+    })
+
+    console.log(state.s)
+  }
+
+  return (
+    <div className="App">
+        <header>
+            <h1>MOVIE DATABASE</h1>
+        </header>
+        <main>
+          <Search handleInput={handleInput} search={search}/>
+          <Results results={state.results} openPopup={openPopup}/>
+          {(typeof state.selected.Title !="undefined") ? <Popup selected={state.selected} closePopup={closePopup}/>:false}
+        </main>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Search = ({handleInput,search}) => {
+    return (
+        <section className="searchbox-wrap">
+            <input type="text" 
+            className="searchbox"
+            placeholder="Search a movie"
+            onChange={handleInput}
+            onKeyPress={search}
+            />
+        </section>
+    )
+}
+
+export default Search
