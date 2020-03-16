35b591ef651cdbc6969ce372b0eba59673052b9b
mozi47 <mozi47.shah@gmail.com>
2020-03-16 14:11:01 +0500

    Updates

---
+++ precache-manifest.a5e0cba20c11ba1db4ffe379b3d77f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cd882492857b9eec461f235d3ef584",
+    "url": "/Movie-Database-React/index.html"
+  },
+  {
+    "revision": "2c57f20758655d0dbcff",
+    "url": "/Movie-Database-React/static/css/main.8679f53c.chunk.css"
+  },
+  {
+    "revision": "b390e3a9b754612ef34b",
+    "url": "/Movie-Database-React/static/js/2.ade12000.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Movie-Database-React/static/js/2.ade12000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c57f20758655d0dbcff",
+    "url": "/Movie-Database-React/static/js/main.c41194ae.chunk.js"
+  },
+  {
+    "revision": "8ab6b1800bb6988af31d",
+    "url": "/Movie-Database-React/static/js/runtime-main.f268cb24.js"
+  }
+]);
